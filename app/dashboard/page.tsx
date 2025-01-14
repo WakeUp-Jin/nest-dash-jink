d3ac77951caab7a458b1fb3edcfefe74c5d68efc
JinK-web <2385472291@qq.com>
2025-01-15 00:15:19 +0800

    fix:修复格式问题

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,6 +9,8 @@ export default async function Page() {
     const latestInvoices = await fetchLatestInvoices();
     const { numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices } = await fetchCardData();
 
+
+
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>

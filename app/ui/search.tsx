0d9f93ef5c8b6e9bf77b9a683a33443bf7f90f89
xjk <xjk@xjkdeMacBook-Pro.local>
2025-01-17 17:42:19 +0800

    feat:学习到11章,搜索和分页

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,30 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  //这个变量是无法修改的
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const { replace } = useRouter()
+
+  function handleSearch(term: string) {
+    console.log(term)
+    //所以这里回重新进行new一个新的对象，这个对象可以进行添加和删除操作
+    const params = new URLSearchParams(searchParams)
+
+    if (term) {
+      params.set('query', term)
+    } else {
+      params.delete('query')
+    }
+
+    console.log("🚀 ~ handleSearch ~ params:", params.toString())
+
+    replace(`${pathname}?${params.toString()}`)
+  }
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +33,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
